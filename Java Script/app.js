6aed4fc802c353e9ba8f0e85bafa02e342a08540
Elizabeth A. Fujita <syp016@c28-0113-18.ad.mtu.edu>
2019-07-12 13:57:24 -0400

    Anna-Voice

---
+++ Java Script/app.js
@@ -0,0 +1,43 @@
+// function add(){
+//     var num1=9;
+//     var num2=3;
+//     var num3;
+//     num3=num2+num1;
+//     alert("the sum is:"+num3)
+// }
+// function subtract(){
+//     var num1=9;
+//     var num2=3;
+//     var num3;
+//     num3=num1-num2;
+//     alert(" the difference is:"+num3)
+// }
+// function multiply(){
+//     var num1=9;
+//     var num2=3;
+//     var num3;
+//     num3=num1*num2;
+//     alert("the product is:"+num3)
+// }
+// function divide(){
+//     var num1=9;
+//     var num2=3;
+//     var num3;
+//     num3=num1/num2;
+//     alert("the quotient is:"+num3)
+// }
+// function stringTest(){
+// var word1="good";
+// var word2="morning";
+// var word3;
+// word3=word1+" "+word2;
+// alert(word3);
+
+// }
+function turnlighton(){
+    document.getElementById('bulb').src="http://w3schools.com/js/pic_bulbon.gif";
+}
+function turnlightoff(){
+    document.getElementById('bulb').src= "http://w3schools.com/js/pic_bulboff.gif";
+    
+}
